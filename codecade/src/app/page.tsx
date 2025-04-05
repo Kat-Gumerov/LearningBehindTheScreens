47442acab0e9e5384ef79ad4309f7e63e2d51420
simone-c18 <151193305+simone-c18@users.noreply.github.com>
2025-04-04 23:52:21 -0400

    updated game selection screen styling

--- codecade/src/app/page.tsx
+++ codecade/src/app/page.tsx
@@ -1,19 +1,36 @@
+'use client'
 import Link from 'next/link'
+import './styles/home.css'
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router} from 'react-router-dom';
+// import { Pixelify_Sans } from 'next/font/google'; //pixel font
+
+// const pixelify = Pixelify_Sans({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    // <div className={pixelify.className}>
     <div>
-      <h1>Welcome to Codecade!!!</h1>
-      <br />
-      {/* game menu */}
-      <h1>Games:</h1>
-      <ul>
-        <Link href='/rockPaperScissors'>
-          <li>Rock Paper Scissors</li>
-        </Link>
-        <li>Hangman</li>
-        <li>Word Scramble</li>
-      </ul>
-    </div>
+      <Router>
+        <Navbar />
+      </Router>
+      {/*game menu*/}
+      <div className='text-center'>
+        <h1 className='title'>GAME SELECT</h1>
+        <br />
+        <ul className='game-select-list space-y-2'>
+          <Link href='/rockPaperScissors'>
+            <li className="game group transition-transform duration-200 ease-in-out transform hover:scale-110 p-2 rounded-lg">Rock Paper Scissors</li>
+          </Link>
+          <Link href='/rockPaperScissors'>
+            <li className="game group transition-transform duration-200 ease-in-out transform hover:scale-110 p-2 rounded-lg">Hangman</li>
+          </Link>
+          <Link href='/rockPaperScissors'>
+            <li className="game group transition-transform duration-200 ease-in-out transform hover:scale-110 p-2 rounded-lg">Word Scramble</li>
+          </Link>
+        </ul>
+        <button className='start-button'>START</button>
+      </div>
+    </div> 
   )
 }
