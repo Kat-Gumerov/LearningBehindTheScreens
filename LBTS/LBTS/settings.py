d32532d585a1da9e102860500b2566f638c13d7f
FernandoAilon <fernandoailon26@yahoo.com>
2025-04-17 00:04:12 -0400

    I fixed a comma issue with the cors allowed origin

--- LBTS/LBTS/settings.py
+++ LBTS/LBTS/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000," # use for production when ready
+    "http://localhost:3000", # use for local development
     "https://learning-behind-the-screens-ppm69z2me.vercel.app/"
 ]
 
