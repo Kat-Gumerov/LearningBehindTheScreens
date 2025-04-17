acde05e31ea67a80e87e16a9a4c9f279fadb6dd9
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-04-17 14:21:51 -0400

    add some more cors settings

--- LBTS/LBTS/settings.py
+++ LBTS/LBTS/settings.py
@@ -110,7 +110,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+ALLOWED_HOSTS=["*"]
+
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", # use for local development
+    "https://learning-behind-the-screens-ppm69z2me.vercel.app",
+    "https://learning-behind-the-scree-git-034218-katrinas-projects-9e7c2dca.vercel.app",
+]
+
+CSRF_TRUSTED_0RIGINS=[
     "http://localhost:3000", # use for local development
     "https://learning-behind-the-screens-ppm69z2me.vercel.app",
     "https://learning-behind-the-scree-git-034218-katrinas-projects-9e7c2dca.vercel.app",
